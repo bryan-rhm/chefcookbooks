410af4d5e23444d9a6f70ba58d3a5d0fc4ff78a6
nicetech <bryan.rhm1@gmail.com>
2018-08-29 11:09:13 -0600

    added redis.conf and sentinel.conf template

--- redis/recipes/default.rb
+++ redis/recipes/default.rb
@@ -33,3 +33,34 @@ execute "install-redis" do
 	command "make && make test && sudo make install"
 	cwd "#{homefolder}/redis-stable"
 end
+
+template "#{homefolder}/redis-stable/redis.conf" do
+	source "redis.rb"
+	mode "0644"
+	master = "NO ONE"
+	port = ""
+	if node["opsworks"]["instance"]["hostname"] == "redis1"
+		master = node["opsworks"]["instance"]["private_ip"]
+		port = "6379"
+	variables(
+		:master_ip => master
+		:master_port => port
+		)
+end
+
+template "#{homefolder}/redis-stable/sentinel.conf" do
+	source "sentinel.rb"
+	layer =node["opsworks"]["instance"]["layers"][0]
+	mode "0644"
+	master = "NO ONE"
+	port = ""
+	node["opsworks"]["layers"]["#{layer}"]["instances"].each  do |instance,properties|
+		if instance == "redis1"
+			master = properties["private_ip"]
+			port = "6379"
+	end
+	variables(
+		:master_ip => master
+		:master_port => port
+		)
+end
