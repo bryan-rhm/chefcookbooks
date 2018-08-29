2ee544c367e7ae3e771fa86c1ed9cd22a78d45e1
nicetech <bryan.rhm1@gmail.com>
2018-08-29 11:30:23 -0600

    separated default into setup and configure recipe

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -1,38 +1,8 @@
 #
 # Cookbook:: redis
-# Recipe:: default
-#
+# Recipe:: configure
+# Author:: Bryan Recinos
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
-#Updating and installing packages
-execute "update" do 
-	command "sudo apt-get update -y"
-	action :run
-end
-
-package "build-essential" do
-	action :install
-end
-
-package "tcl" do
-	action :install
-end
-
-package "libjemalloc-dev" do
-	action :install
-end
-
-#Download and install redis
-homefolder = "/home/ubuntu"
-execute "download-redis" do
-	command "curl -O http://download.redis.io/redis-stable.tar.gz && tar xzvf redis-stable.tar.gz"
-	cwd "#{homefolder}"
-end
-
-execute "install-redis" do
-	command "make && make test && sudo make install"
-	cwd "#{homefolder}/redis-stable"
-end
 
 template "#{homefolder}/redis-stable/redis.conf" do
 	source "redis.rb"
