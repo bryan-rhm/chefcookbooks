43e0acb7dfc1b8f033bf5fb52f8e8bc930c7ccc6
nicetech <bryan.rhm1@gmail.com>
2018-08-29 11:16:16 -0600

    solved error on variables

--- redis/recipes/default.rb
+++ redis/recipes/default.rb
@@ -43,7 +43,7 @@ template "#{homefolder}/redis-stable/redis.conf" do
 		master = node["opsworks"]["instance"]["private_ip"]
 		port = "6379"
 	variables(
-		:master_ip => master
+		:master_ip => master,
 		:master_port => port
 		)
 end
@@ -60,7 +60,7 @@ template "#{homefolder}/redis-stable/sentinel.conf" do
 			port = "6379"
 	end
 	variables(
-		:master_ip => master
+		:master_ip => master,
 		:master_port => port
 		)
 end
